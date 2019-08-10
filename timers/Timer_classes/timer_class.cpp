e9ac0efe7ca5c637c42b67c9115bb50d896bbaeb
Veronica <veronikasuper1@gmail.com>
2019-08-11 01:10:13 +1000

    Made classes

---
+++ timers/Timer_classes/timer_class.cpp
@@ -0,0 +1,84 @@
+#include "timer_class.h"  
+
+volatile uint64_t isr_milliseconds;
+
+ISR(TIMER1_COMPA_vect)
+{
+isr_milliseconds++;
+}
+
+ //Constructor
+ Timer::Timer(uint32_t Period,  uint32_t Unit){
+        period = Period*Unit;
+        milliseconds = 0;
+        expirations = 0;
+    }
+
+
+    //Start function
+   void Timer::startTimer(uint8_t Timer_name){
+  cli();
+  isr_milliseconds = 0;
+  switch(Timer_name){
+    case TIMER_0:
+    TIMSK0 = (1<<OCIE0A);
+    TCCR0A=(1<<WGM01);
+    TCCR0B=(1<<CS01) | (1<<CS00);
+    OCR0A=0xF9;
+    break;
+    case TIMER_1:
+ // TCCR1A = 0; TCCR1B = 0;
+    TIMSK1 = (1<<OCIE1A);
+    TCCR1B = (1<<WGM12)|(1<<CS10)|(1<<CS11); //CTC, 64 div
+    OCR1A = 250;  //max period 0.26214 sec = (64)/(16 MHz) * 65535 / OCR1A = 250 -> 0.001 sec
+    break;
+    case TIMER_2:
+    TIMSK2=(1<<OCIE2A);
+    TCCR2A=(1<<WGM21);
+    TCCR2B=(1<<CS22);
+    TCNT2=0x00;
+    OCR2A=0xF9;
+    break;
+  }
+  sei();
+}
+
+void Timer::stopTimer(){
+  cli();
+
+  period = 0;
+  milliseconds = 0;
+  expirations = 0;
+  TIMSK0 = 0;
+  TCCR0A = TCCR0B = 0;
+  OCR0A=0;
+
+  TIMSK1 = 0;
+  TCCR1A = TCCR1B = 0;
+  OCR1A = 0;
+
+  TIMSK2 = 0;
+  TCCR2A = TCCR2B = 0;
+  OCR2A = 0;
+  }
+
+uint32_t Timer::getExp() {
+  if (period != 0) {
+    if ((isr_milliseconds-milliseconds)/period >= 1){
+    expirations = expirations + (isr_milliseconds-milliseconds)/period;
+    milliseconds = isr_milliseconds;
+    }
+    return expirations; 
+  } else return 0;
+}
+
+uint32_t Timer::getMillis() {
+  return isr_milliseconds;
+}
+
+uint32_t Timer::getPeriods(uint32_t Unit) {
+  return getExp()*period/Unit;
+}
+
+
+
---
+++ timers/Timer_classes/timer_class.h
@@ -0,0 +1,28 @@
+#ifndef timer_class_h_
+#define timer_class_h_
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <inttypes.h>
+
+#define TIMER_0 0
+#define TIMER_1 1
+#define TIMER_2 2
+
+#define MILLIS 1
+#define SECS 975    //must be 1000
+#define MINUTES 58500   //must be 60000, but my arduinos "minute" is 1 min + 1.5 sec
+#define HOURS 3600000
+
+class Timer{
+private:
+ 
+public:
+    uint64_t period, milliseconds, expirations;
+    Timer(uint32_t,  uint32_t);
+    void startTimer(uint8_t);
+    void stopTimer();
+    uint32_t getExp();
+    uint32_t getMillis();
+    uint32_t getPeriods(uint32_t);
+};
+#endif /*timer_class_h_ */
