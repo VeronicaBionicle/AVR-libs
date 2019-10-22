3c66d9be9bb6583255fc74f81a5859941eadf60a
Veronica <veronikasuper1@gmail.com>
2019-10-22 23:18:23 +1000

    Added  ATmega64 and  fixed defines for interrupt vector

--- impulse_control/ImpulseControl.h
+++ impulse_control/ImpulseControl.h
@@ -7,27 +7,32 @@
 	#define INTERRUPT_FLAG GIFR
 	#define EXTERNAL_INTERRUPT_PORT DDRB
 	#define EXTERNAL_INTERRUPT_PIN_0 1
+	#define EXT_INT INT0_vect
 #elif defined (__AVR_ATmega8__)
 	#define INTERRUPT_INIT MCUCR
 	#define GENERAL_INTERRUPT GIMSK
 	#define INTERRUPT_FLAG GIFR
 	#define EXTERNAL_INTERRUPT_PORT DDRD
 	#define EXTERNAL_INTERRUPT_PIN_0 2
+	#define EXT_INT INT0_vect
+#elif defined (__AVR_ATmega64__)
+	#define INTERRUPT_INIT EICRA
+	#define GENERAL_INTERRUPT EIMSK
+	#define INTERRUPT_FLAG EIFR
+	#define EXTERNAL_INTERRUPT_PORT DDRD
+	#define EXTERNAL_INTERRUPT_PIN_0 0
+	#define EXT_INT INT0_vect
 #elif defined (__AVR_ATmega328P__)
 	#define INTERRUPT_INIT EICRA
 	#define GENERAL_INTERRUPT EIMSK
 	#define INTERRUPT_FLAG EIFR
 	#define EXTERNAL_INTERRUPT_PORT DDRD
 	#define EXTERNAL_INTERRUPT_PIN_0 2
+	#define EXT_INT INT0_vect
 #endif
-
-/*	Zerocross (external interrupt) registers	*/
-
 
 /*	Zerocross pin selection	*/
 #define ZEROCROSS_PIN EXTERNAL_INTERRUPT_PIN_0
-#define EXT_INT INT0_vect
-ISR (EXT_INT);
 
 /*	Output pin selection	*/
 #define OUT_DDR DDRB
