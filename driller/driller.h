24238d2be6b85e811af6b96ff497bc6c83533ec4
Veronica <veronikasuper1@gmail.com>
2020-01-21 17:52:26 +1000

    Added and tested halfstep modes and making the full turn.

---
+++ driller/driller.h
@@ -0,0 +1,62 @@
+#ifndef driller_h_
+#define driller_h_
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <inttypes.h>
+#include "timer_class.h"
+
+#define MAX_FULLSTEP_STAGES 3
+#define MAX_HALFSTEP_STAGES 7
+#define FULL_STEPS_PER_ROTATION int(360/7.5) //7.5 degrees per step
+#define HALF_STEPS_PER_ROTATION int(2*360/7.5) //3.75 degrees per step
+/*Modes of driver*/
+#define FORWARD_FULLSTEP 1
+#define REVERSE_FULLSTEP 2
+#define FORWARD_HALFSTEP 3
+#define REVERSE_HALFSTEP 4
+#define STOP 0
+
+void StepperMode(uint8_t mode, uint16_t rotate_time);   //function to choose mode of work
+void ForwardFullStep();
+void ReverseFullStep();
+void ForwardHalfStep();
+void ReverseHalfStep();
+void Stop();
+uint16_t MadeSteps();   //returns the number of steps 
+void MadeSteps(uint16_t new_number);    //defines new number of steps
+
+/*State tables for steps*/
+const uint8_t forward_stage_fullstep[4] = {   //-A, -B, A, B
+  0b00000011,
+  0b00001001,
+  0b00001100,
+  0b00000110
+};
+const uint8_t reverse_stage_fullstep[4] = {   //-A, -B, A, B
+  0b00000011,
+  0b00000110,
+  0b00001100,
+  0b00001001
+};
+const uint8_t forward_stage_halfstep[8] = {   //-A, -B, A, B
+  0b00000010,
+  0b00000011,
+  0b00000001,
+  0b00001001,
+  0b00001000,
+  0b00001100,
+  0b00000100,
+  0b00000110
+};
+const uint8_t reverse_stage_halfstep[8] = {   //-A, -B, A, B
+  0b00000001,
+  0b00000011,
+  0b00000010,
+  0b00000110,
+  0b00000100,
+  0b00001100,
+  0b00001000,  
+  0b00001001
+};
+#endif /*driller_h_ */
--- driller/driller.ino
+++ driller/driller.ino
@@ -1,65 +1,26 @@
 #include "timer_class.h"
+#include "driller.h"
 
-#define MAX_STAGES 3
 #define ROTATE_TIME 10000
-Timer timer1;
-uint8_t current_stage;
-uint8_t reverse = 0;
 
-uint8_t forward_stage[4] = {   //-A, -B, A, B active high
-  0b00000011,
-  0b00001001,
-  0b00001100,
-  0b00000110
-};
-
-uint8_t forward_stage_low[4] = {   //-A, -B, A, B active low
-  0b00001100,
-  0b00000110,
-  0b00000011,
-  0b00001001
-};
-
-uint8_t reverse_stage[4] = {   //-A, -B, A, B
-  0b00000011,
-  0b00000110,
-  0b00001100,
-  0b00001001
-};
+//FULL_STEPS_PER_ROTATION int(360/7.5) //48
+//HALF_STEPS_PER_ROTATION int(2*360/7.5) //96
 
 void setup() {
-  DDRB = 0x0F;  //PB0 (B), PB1 (A), PB2 (-B), PB3 (-A), PB4 - Reverse button
+  DDRB = (1 << PB0) | (1 << PB1) | (1 << PB2) | (1 << PB3); //PB0 (B), PB1 (A), PB2 (-B), PB3 (-A), PB4 - Reverse button
+  StepperMode(FORWARD_HALFSTEP, ROTATE_TIME);
+ // StepperMode(FORWARD_FULLSTEP, ROTATE_TIME);
 }
 
-void Forward() {
-  PORTB = forward_stage[current_stage];
-  if (current_stage < MAX_STAGES) {current_stage = current_stage + 1;} else {current_stage = 0;}
-};
-
-void Reverse() {
-  PORTB = reverse_stage[current_stage];
-  if (current_stage < MAX_STAGES) {current_stage++;} else {current_stage = 0;}
-};
-
 void loop() {
-      if (PINB & (1<<PB4)) { // если нажата
-          if (!reverse) {
-            timer1.stopTimerCounter();
-            PORTB = 0;
-            delay(500);
-            timer1.attachTimerInterrupt(Reverse, ROTATE_TIME);
-            reverse = 1;
-            current_stage = 0;
-            } 
-        } else {
-        if (reverse) {
-          
-          timer1.stopTimerCounter();
-          PORTB = 0;
-          delay(500);
-          timer1.attachTimerInterrupt(Forward, ROTATE_TIME);
-          reverse = 0;
-          current_stage = 0;
-          }
-        };  
+  if (MadeSteps() == HALF_STEPS_PER_ROTATION) {
+    Stop();
+    MadeSteps(0);
+    delay(1000);
+    if (PINB & (1 << PB4)) {
+      StepperMode(REVERSE_HALFSTEP, ROTATE_TIME);
+    } else {
+      StepperMode(FORWARD_HALFSTEP, ROTATE_TIME);
+    };
+  }
 }
